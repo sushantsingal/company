a1d45fa2b5804ce75f42379a15e58bf7e0477fd8
Sushant Singal <sushantsingal42@gmail.com>
2025-07-23 23:00:39 +0530

    ..

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://marketing-crawlers.onrender.com', // backend server
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
