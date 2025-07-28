bb231f3c69b78c7755956f6841a645d0969b722a
Sushant Singal <sushantsingal42@gmail.com>
2025-07-28 16:21:20 +0530

    logo

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,17 +1,18 @@
 import hero from "../assets/hero-image.jpg";
+import { BsTwitterX } from "react-icons/bs";
 import {
   Facebook,
-  Twitter,
   Mail,
   Phone,
   Instagram,
   Linkedin,
   MessageCircle,
+  LinkedinIcon,
 } from "lucide-react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white py-12 px-6 md:px-20">
+    <footer className="bg-gray-50 text-gray-800 py-12 px-6 md:px-20">
       <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-10">
         {/* Column 1 */}
         <div>
@@ -77,13 +78,13 @@ const Footer = () => {
               <Facebook size={18} />
             </a>
             <a href="#" className="text-gray-500 hover:text-pink-600">
-              <Twitter size={18} />
+              <BsTwitterX size={18} />
             </a>
             <a href="#" className="text-gray-500 hover:text-pink-600">
               <Instagram size={18} />
             </a>
             <a href="#" className="text-gray-500 hover:text-pink-600">
-              <MessageCircle size={18} />
+              <LinkedinIcon size={18} />
             </a>
           </div>
         </div>
