3503fdaa66bc6ead6dc346ab1f99b863f837f218
Sushant Singal <sushantsingal42@gmail.com>
2025-08-07 13:21:47 +0530

    s

--- client/src/components/Topbar.jsx
+++ client/src/components/Topbar.jsx
@@ -40,7 +40,7 @@ const pages = [
   { name: "Terms", path: "/terms" },
   { name: "Advisors", path: "/advisors" },
   { name: "Mobile App Development", path: "/mobile-dev" },
-  { name: "Web Development", path: "/web-dev" },
+  { name: "Website Development", path: "/web-dev" },
 ];
 
 const Topbar = () => {
